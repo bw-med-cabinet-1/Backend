f39c722d5ae999bf60f14e7d2b578373a77dc882
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-09-21 19:37:00 -0400

    preparing to deploy

--- data/db-config.js
+++ data/db-config.js
@@ -3,5 +3,6 @@ const knex = require('knex');
 const knexfile = require('../knexfile');
 
 const enviroment = process.env.NODE_ENV || 'development'
+//DB_ENV
 
 module.exports=knex(knexfile[enviroment]);
--- knexfile.js
+++ knexfile.js
@@ -33,18 +33,16 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {          
+      filename: './data/database.db3', // where to change the name of the database 
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
 };
